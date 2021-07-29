0f0642b7c2532f5e87b3582b6f32a66a16d8bf05
Tetsuya Idota <tidota@hawaii.edu>
2021-07-28 20:35:51 -1000

    Subscribe scan and publish cmd_vel

--- src/controller.cpp
+++ src/controller.cpp
@@ -1,68 +1,85 @@
 // controller.cpp
 
-// TODO: modify the sample code below so that it performs the simple controller.
-
 // the code was made by combining the two pieces of the sample code from
 // https://docs.ros.org/en/foxy/Tutorials/Writing-A-Simple-Cpp-Publisher-And-Subscriber.html
 
 #include <chrono>
 #include <functional>
 #include <memory>
+#include <mutex>
 #include <string>
 
 #include <rclcpp/rclcpp.hpp>
-#include <std_msgs/msg/string.hpp>
+#include <geometry_msgs/msg/twist.hpp>
+//#include <std_msgs/msg/string.hpp>
+#include <sensor_msgs/msg/laser_scan.hpp>
 
 using namespace std::chrono_literals;
 
 using std::placeholders::_1;
 
-class MinimalPubSub : public rclcpp::Node
+class WallFollower : public rclcpp::Node
 {
   public:
-    MinimalPubSub()
-    : Node("minimal_pubsub"), count_(0)
+    WallFollower(): Node("wall_follower")
     {
-      publisher_
-        = this->create_publisher<std_msgs::msg::String>("topic", 10);
-
+      // Subscribe the topic for proximity data
       subscription_
-        = this->create_subscription<std_msgs::msg::String>(
-          "topic", 10, std::bind(&MinimalPubSub::topic_callback, this, _1));
+        = this->create_subscription<sensor_msgs::msg::LaserScan>(
+          "scan", 10, std::bind(&WallFollower::topic_callback, this, _1));
+
+      // Make a publisher for controll
+      publisher_
+        = this->create_publisher<geometry_msgs::msg::Twist>("cmd_vel", 10);
 
       timer_
         = this->create_wall_timer(
-          500ms, std::bind(&MinimalPubSub::timer_callback, this));
+          500ms, std::bind(&WallFollower::timer_callback, this));
     }
 
-  private: rclcpp::Publisher<std_msgs::msg::String>::SharedPtr publisher_;
-  private: rclcpp::Subscription<std_msgs::msg::String>::SharedPtr subscription_;
-  private: rclcpp::TimerBase::SharedPtr timer_;
-  private: size_t count_;
+  private:
+    rclcpp::Publisher<geometry_msgs::msg::Twist>::SharedPtr publisher_;
+  private:
+    rclcpp::Subscription<sensor_msgs::msg::LaserScan>::SharedPtr subscription_;
+  private:
+    rclcpp::TimerBase::SharedPtr timer_;
+
+  private: std::mutex scan_mutex;
+  private: sensor_msgs::msg::LaserScan scan_msg_buff;
 
   private: void topic_callback(
-                  const std_msgs::msg::String::SharedPtr msg) const;
+                  const sensor_msgs::msg::LaserScan::SharedPtr msg);
   private: void timer_callback();
 };
 
-void MinimalPubSub::topic_callback(
-  const std_msgs::msg::String::SharedPtr msg) const
+void WallFollower::topic_callback(
+  const sensor_msgs::msg::LaserScan::SharedPtr msg)
 {
-  RCLCPP_INFO(this->get_logger(), "I heard: '%s'", msg->data.c_str());
+  std::lock_guard<std::mutex> lk(scan_mutex);
+  scan_msg_buff = *msg;
 }
 
-void MinimalPubSub::timer_callback()
+void WallFollower::timer_callback()
 {
-  auto message = std_msgs::msg::String();
-  message.data = "Hello, world! " + std::to_string(count_++);
-  RCLCPP_INFO(this->get_logger(), "Publishing: '%s'", message.data.c_str());
+  RCLCPP_INFO(this->get_logger(), "Publishing command");
+  auto message = geometry_msgs::msg::Twist();
+
+  sensor_msgs::msg::LaserScan msg;
+  {
+    std::lock_guard<std::mutex> lk(scan_mutex);
+    msg = scan_msg_buff;
+  }
+
+  // TODO: make a command message based on the sensor
+  message.angular.z = 1.0;
+
   publisher_->publish(message);
 }
 
 int main(int argc, char * argv[])
 {
   rclcpp::init(argc, argv);
-  rclcpp::spin(std::make_shared<MinimalPubSub>());
+  rclcpp::spin(std::make_shared<WallFollower>());
   rclcpp::shutdown();
   return 0;
 }
